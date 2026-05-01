bf5cfe01e353e38186022873d225d43b8a24a75a
Al-DEB <salamsido1996@gmail.com>
2026-05-01 19:22:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const TelegramBot = require("node-telegram-bot-api");
-const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
-bot.sendMessage(process.env.CHAT_ID, "Test bot working");
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Hello from test bot");
-});
-console.log("Bot started");
+
